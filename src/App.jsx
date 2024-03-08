0750ee8cff2e3a3a5b3b84b52358c651f582dc39
saidulislam28 <saidulislam79028@gmail.com>
2024-03-08 22:42:16 +0600

    first react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
+import Friends from './Friends'
+
+function App() {
+  
+
+  return (
+    <>
+     <h3>React core concepts</h3>
+     <Friends></Friends>
+
+
+     <Users></Users>
+     <Team></Team>
+     <Counter></Counter>
+    </>
+
+
+  )
+}
+
+export default App
